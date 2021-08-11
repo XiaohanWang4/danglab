fc90c1bd28ff338d22728562da643087a393dd86
Xiaohan Wang <72052891+XiaohanWang4@users.noreply.github.com>
2021-08-11 13:19:22 +0800

    145354
    2323

--- src/Component/Research/index.js
+++ src/Component/Research/index.js
@@ -35,9 +35,6 @@ const Research = () => {
             <div className="mission">
                 <h1>Mission: We employ advanced approaches to develop cutting-edge protein based technologies and protein therapeutics.  </h1>
             </div>
-            <div className="">
-                <h1></h1>
-            </div>
             <div className="research-content">
             Fascinated by protein molecules, Dr. Dang has been focusing his researches on studying the structure, function and biological activities of protein molecules through chemical synthesis, computational design and biological display technologies. Chemical protein synthesis provides atomic control of protein structures and enables protein modifications in unprecedented ways. Computational protein design allows protein structure exploration beyond natural world. We walk at the frontier of protein synthesis and protein design to develop technologies and to advance protein therapeutics with the help of high throughput protein screening. 
             </div>
