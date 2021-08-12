4630ab7df54cfef5767ae7c421023df115f450dd
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2021-08-12 20:30:33 +0800

    modify

--- src/Component/Menu/index.js
+++ src/Component/Menu/index.js
@@ -1,15 +1,26 @@
 import "./index.css";
-
+import { Link } from 'react-router-dom';
 const Menu = () => {
     return (
         <div className="menu-container">
-
-        <span onClick={() => window.location.href="/"} className="url">Home</span> 
-        <span onClick={() => window.location.href="/research"} className="url">Research</span>
-        <span onClick={() => window.location.href="/team"} className="url">Team</span>
-        <span onClick={() => window.location.href="/publication"} className="url">Publication</span>
-        <span onClick={() => window.location.href="/join"} className="url">Join us</span>
-        <span onClick={() => window.location.href="/resources"} className="url">Resources</span>
+            <Link to="/">  
+                <span className="url">Home</span>
+            </Link>
+            <Link to="/research">  
+                <span className="url">Research</span>
+            </Link>
+            <Link to="/team">  
+                <span className="url">Team</span>
+            </Link>
+            <Link to="/publication">  
+                <span className="url">Publication</span>
+            </Link>
+            <Link to="/join">  
+                <span className="url">Join us</span>
+            </Link>
+            <Link to="/resources">  
+                <span className="url">Resources</span>
+            </Link>
         </div>
     )
 }
